dab4c78370fdade152535f61e2df4f9fb3adae6f
Joshua Smee <smeejosh81@gmail.com>
2022-02-05 22:45:01 +1100

    Moved api to new repository

---
+++ config.py
@@ -0,0 +1,19 @@
+from os import environ, getcwd, path
+
+port = 5000
+
+url = f"http://localhost:{port}/"
+
+class Config:
+    """Set Flask configuration from .env file."""
+
+    # General Config
+    # SECRET_KEY = environ.get('SECRET_KEY') DEAL WITH THIS LATER
+
+    # Database
+    
+    directory = getcwd()
+    
+    SQLALCHEMY_DATABASE_URI = f"sqlite:////{directory}/UpAndDownTheRiverMultiplayer.db"
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
